a1c0d4ebba1f8c812cb5bcec780150d0d0ccab02
Huynh Trong Nghia <huynhtrongnghia15n@gmail.com>
2026-01-23 14:51:37 +0700

    feat(application): implement UpdateProductCommand and Handler
    - Implement UpdateProductCommandHandler to fetch, map, and persist changes.
    - Use MediatR Unit type for command response.

---
+++ src/TechShop.ECommerce.Application/Features/Product/Commands/UpdateProduct/UpdateProductCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace TechShop.ECommerce.Application.Features.Product.Commands.UpdateProduct;
+
+public class UpdateProductCommand : IRequest<Unit>
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Summary { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+    public int CategoryId { get; set; }
+}
---
+++ src/TechShop.ECommerce.Application/Features/Product/Commands/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace TechShop.ECommerce.Application.Features.Product.Commands.UpdateProduct;
+
+public class UpdateProductCommandHandler(
+    IMapper mapper,
+    IProductRepository productRepository) : IRequestHandler<UpdateProductCommand, Unit>
+{
+    public async Task<Unit> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+    {
+        var productToUpdate = mapper.Map<TechShop.ECommerce.Domain.Entities.Product>(request);
+
+        await productRepository.UpdateAsync(productToUpdate);
+
+        return Unit.Value;
+    }
+}
