9946e102cc12151c42b18875494d2df7ca721407
Huynh Trong Nghia <huynhtrongnghia15n@gmail.com>
2026-01-24 21:16:37 +0700

    feat(infrastructure): setup email service registration and global usings

---
+++ src/TechShop.ECommerce.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,8 @@
+﻿global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Options;
+global using SendGrid;
+global using SendGrid.Helpers.Mail;
+global using TechShop.ECommerce.Application.Contracts.Email;
+global using TechShop.ECommerce.Application.Models.Email;
+global using TechShop.ECommerce.Infrastructure.EmailService;
---
+++ src/TechShop.ECommerce.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,12 @@
+﻿namespace TechShop.ECommerce.Infrastructure;
+
+public static class InfrastructureServicesRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailSender, EmailSender>();
+
+        return services;
+    }
+}
