3f906c98e8c5539726dcefd5bab628509b532b7d
Huynh Trong Nghia <huynhtrongnghia15n@gmail.com>
2026-01-31 19:45:52 +0700

    feat(identity): add application user entity
    - Create ApplicationUser inheriting from IdentityUser
    - Extend default identity user for future custom fields
    - Prepare for Identity EF Core integration

---
+++ src/Infrastructure/TechShop.ECommerce.Identity/Models/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿namespace TechShop.ECommerce.Identity.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
