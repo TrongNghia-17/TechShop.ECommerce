1c579e1051fab015a157f294c796567aad0e9926
Huynh Trong Nghia <huynhtrongnghia15n@gmail.com>
2026-01-31 21:31:36 +0700

    feat(api): add authentication controller
    - Add AuthController for authentication endpoints
    - Expose login and register APIs
    - Delegate authentication logic to IAuthService
    - Keep API layer thin and focused on HTTP concerns

---
+++ src/API/TechShop.ECommerce.Api/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿namespace TechShop.ECommerce.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController(
+    IAuthService authenticationService)
+    : ControllerBase
+{
+    [HttpPost("login")]
+    public async Task<ActionResult<AuthResponse>> Login(AuthRequest request)
+    {
+        return Ok(await authenticationService.Login(request));
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+    {
+        return Ok(await authenticationService.Register(request));
+    }
+}
