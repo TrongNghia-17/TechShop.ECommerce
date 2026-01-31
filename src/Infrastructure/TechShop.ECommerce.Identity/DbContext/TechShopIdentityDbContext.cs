e2c68f06a55715baef05fa9a49062539f8b27302
Huynh Trong Nghia <huynhtrongnghia15n@gmail.com>
2026-01-31 21:29:13 +0700

    feat(identity): add identity database context
    - Add TechShopIdentityDbContext inheriting from IdentityDbContext
    - Configure ApplicationUser as default identity user
    - Prepare identity database schema for authentication and authorization

---
+++ src/Infrastructure/TechShop.ECommerce.Identity/DbContext/TechShopIdentityDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace TechShop.ECommerce.Identity.DbContext;
+
+public class TechShopIdentityDbContext(
+    DbContextOptions<TechShopIdentityDbContext> options)
+    : IdentityDbContext<ApplicationUser>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(TechShopIdentityDbContext).Assembly);
+    }
+}
